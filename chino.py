804c4d9ebaff01910d527383b824f1c400dbc4a4
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-13 13:53:59 +0900

    画像を表示する

--- chino.py
+++ chino.py
@@ -8,3 +8,7 @@ def imageToData(filename):
     #画像を8*8のグレースケールに変換
     grayImage = PIL.Image.open(filename).convert("L")
     grayImage = grayImage.resize((8,8),PIL.Image.ANTIALIAS)
+    #その画像を表示する
+    dispImage = PIL.ImageTk.PhotoImage(grayImage.resize((300,300)))
+    imageLabel.configure(image = dispImage)
+    imageLabel.image = dispImage
