8f4d47954b263235c3a00820e7702c4784765eaa
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-13 13:51:06 +0900

    画像をグレーに変換

--- chino.py
+++ chino.py
@@ -5,4 +5,6 @@ import PIL.ImageTk
 
 #画像ファイルを数値リストに変換する
 def imageToData(filename):
-    
+    #画像を8*8のグレースケールに変換
+    grayImage = PIL.Image.open(filename).convert("L")
+    grayImage = grayImage.resize((8,8),PIL.Image.ANTIALIAS)
